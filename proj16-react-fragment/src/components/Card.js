e101833cdf83eeb45fdfe688db96a46121e54d78
asdethyg <arina.fedorova@inlinegroup-c.ru>
2023-05-17 18:24:32 +0300

    fragment

---
+++ proj16-react-fragment/src/components/Card.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Card = () => {
+  return (
+    // <React.Fragment>
+    <>
+      <h1>Something</h1>
+      <h3>Something Else</h3>
+      <button>Click</button>
+    </>
+    // </React.Fragment>
+  )
+}
+export default Card
